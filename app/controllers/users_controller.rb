ab58dffa8fd40d92ce47e1b77cfcb64515d75da6
kohei15 <101j00113@gmail.com>
2019-03-07 22:14:48 +0900

    ユーザー編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,25 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
 	end
 
 	def show
+		@user = current_user
 	end
 
 	def edit
+		@user = current_user
 	end
 
 	def update
+		@user = current_user
+		if @user.update(user_params)
+			flash[:notice] = "変更内容を更新しました"
+			sign_in(@user, :bypass => true)
+			redirect_to root_path
+		else
+			render :edit
+		end
 	end
 
 	def new
@@ -18,5 +29,10 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'smoothies#top'
-  resource :foods
-  resource :smoothies
-  resource :users
+  resource :user
+  resources :foods
+  resources :smoothies
 
   devise_for :users, controllers: {
   	sessions:      'users/sessions',
