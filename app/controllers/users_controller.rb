7860a0cd57f051fab2dd99fb800a487e62ec5c83
kohei15 <101j00113@gmail.com>
2019-03-09 19:20:07 +0900

    スムージーの一覧・詳細ページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all.order(created_at: "desc")
 	end
 
 	def show
+		
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		user = User.find(params[:id])
+    	user.update(user_params)
+    	redirect_to user_path(user.id)
 	end
 
 	def new
@@ -18,5 +24,17 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+	def corrent_user
+		user = User.find(params[:id])
+		if corrent_user != user
+			redirect_to root_path
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :email, :encrypted_password)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'smoothies#top'
-  resource :foods
-  resource :smoothies
-  resource :users
+  resources :foods
+  resources :smoothies
+  resource :user, except:[:index]
+  resources :users, only:[:index]
 
-  devise_for :users, controllers: {
-  	sessions:      'users/sessions',
-  	passwords:     'users/passwords',
-  	registrations: 'users/registrations'
+   devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
   }
+
 end
