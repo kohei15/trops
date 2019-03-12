746c5de49de3eeeed68c3014afd566bbcc4a6eeb
kohei15 <101j00113@gmail.com>
2019-03-12 20:42:12 +0900

    スムージーのカスタマイズページを作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -17,3 +18,19 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+function spinner(counter){
+    var step = 0.25;
+    var min = 0.0;
+    var max = 10.0;
+
+    var food_quantity = $(".food_quantity").val();
+    var food_quantity = parseFloat(food_quantity);
+      if (counter == 'up') { food_quantity += step; };
+      if (counter == 'down') { food_quantity -= step; };
+
+    if ( food_quantity < min ) { food_quantity = min; };
+    if ( max < food_quantity ) { food_quantity = max; };
+
+    $(".food_quantity").val(food_quantity);
+}
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
 		smoothie = Smoothie.find(params[:smoothy_id])
 		@favorite = current_user.favorites.new(smoothie_id: smoothie.id)
 		@favorite.save
-		redirect_to smoothies_path
+		redirect_to smoothy_path(smoothie.id)
 	end
 
 	def destroy
 		smoothie = Smoothie.find(params[:smoothy_id])
 		@favorite = current_user.favorites.find_by(smoothie_id: smoothie.id)
 		@favorite.destroy
-		redirect_to smoothies_path
+		redirect_to smoothy_path(smoothie.id)
 	end
 end
