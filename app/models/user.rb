d1866c63114870d7217c764eb909e32cdd023e8e
kohei15 <101j00113@gmail.com>
2019-03-12 14:18:22 +0900

    お気に入り登録機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :smoothies
-  has_many :favorites
+  has_many :favorites,dependent: :destroy
 
   validates :name, presence: true
 
   # 管理者権限
   enum role: {user: 0, admin: 1}
+
+  def favorities_by?(user)
+  	favorites.where(user_id: user.id).exists?
+  end
 end
